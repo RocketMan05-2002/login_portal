573431e5bfa025380a8dc341fdebe4f7d53accb2
Rocketman <abhinayshambharkar1@gmail.com>
2025-03-17 23:57:30 +0530

    format fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login/Login'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <Router>
-        <Link to="/login">
+      <Link to="/login">
         <button>Login</button>
-        </Link>
-        <button>Sign up</button>
+      </Link>
+      <button>Sign up</button>
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
